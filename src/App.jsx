1abe77bf318da8f9732e62f31462dd02f7618e19
Josafar Garcia <josafatgarciasarmientos@gmail.com>
2024-03-26 21:13:22 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+function App(){
+
+    return(
+
+        <div className="main">
+            <div className="container">
+                <img src="./logo.png" alt="logo" className="logoImage"/> 
+            </div>   
+            <div className="main__cursors">
+                <div className="cursor__1" >
+                    <div className="cursor__2" >
+                        <div className="cursor__3" >
+                            <div className="cursor__4" >
+                                <div className="cursor__5" >
+                                    <h1>AirShare</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="button-container">
+                <a href="#" className="donate">Donate</a>
+                <a href="#"><img src="./github_link.svg" alt="github icon" /></a>
+            </div>
+        </div>
+       
+    );
+}
+
+export default App;
